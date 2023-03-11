743a5dce86e6445c3ebe399305709a88f35f8469
Valentino Traverso <vt20032003@live.com.ar>
2023-03-11 13:33:25 +0100

    feat(alert require login): change text & set navigate to login

--- src/views/components/PopUp/RequireLogedAlert.jsx
+++ src/views/components/PopUp/RequireLogedAlert.jsx
@@ -1,32 +1,21 @@
-import { logoutUser } from '../../../helpers/utils/userUtils';
-import useSession from '../../../helpers/hooks/useSession';
+import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 
 export default function LogOutAlert({setAlertLogOut}) {
-    const {verifyLogin} = useSession();
-
     const MySwal = withReactContent(Swal)
+    const navigate = useNavigate();
 
     MySwal.fire({
-        title: 'Are you sure?',
-        icon: 'warning',
+        title: 'You need to be loged to add products to the cart',
+        icon: 'info',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, i want to logout.'
+        confirmButtonText: 'Login'
     }).then((result) => {
         if (result.isConfirmed) {
-            MySwal.fire(
-                'Good Bye!👋',
-            )
-            logoutUser();
-
-            verifyLogin();
-
-            setAlertLogOut(false);
-        }else{
-            setAlertLogOut(false);
+            navigate('/login')
         }
     })
 }
